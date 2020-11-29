85997b1b96aaf47c93197f88519bc1933bda0c64
Ajmeer0007 <70527935+Ajmeer0007@users.noreply.github.com>
2020-11-29 15:47:21 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,94 @@
+
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Medical Management System</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<style>
+		.error {color: #FF0000;}
+	</style>
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="header_top">
+			<!-- <a href="index.php"><img src="img/dash2.jpg" alt="logo.png img"></a> -->
+
+			<span style="font-size:50px;color:#2c2f84;font-weight:bolder;margin-left:15px;">Doctor Appoinment System</span>
+		</div>
+
+	<!-- 	this is for menu -->
+	<div class="navbar navbar-default nav">
+		<nav class="menu">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="doctorinfo.php">Medical Doctors</a></li>
+				<li><a href="about.php">About Us</a></li>
+				
+			</ul>
+		</nav>
+	</div>
+	
+
+
+
+
+	<!-- this is for donor registraton -->
+	<div class="main_content" style="background-color:#fff;">
+		<h3 class="text-center" style="background-color:#272327;color: #fff;">About Us</h3>
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<img src="img/aboutimag33.jpg" alt="" class="img-responsive">
+			</div>
+
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">We are Beside you</h3>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt et dolores neque eaque. Labore, dicta similique ipsam odit saepe obcaecati suscipit ducimus, commodi dolores animi sed beatae, quia nulla facilis.lorem 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, doloribus quisquam eaque inventore consectetur sapiente itaque aliquid quae tempore. Eveniet iusto obcaecati magni qui laboriosam saepe unde, totam odio excepturi
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, doloribus quisquam eaque inventore consectetur sapiente itaque aliquid quae tempore. Eveniet iusto obcaecati magni qui laboriosam saepe unde, totam odio excepturi.</p>
+				</article>
+			</div>
+		</div>
+
+
+		<div class="col-md-12">
+			<div class="col-md-6">
+				<article>
+					<h3 style="color:#0616BC;">24/7 hour service</h3>
+					<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt et dolores neque eaque. Labore, dicta similique ipsam odit saepe obcaecati suscipit ducimus, commodi dolores animi sed beatae, quia nulla facilis.lorem 
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, doloribus quisquam eaque inventore consectetur sapiente itaque aliquid quae tempore. Eveniet iusto obcaecati magni qui laboriosam saepe unde, totam odio excepturi
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Totam, doloribus quisquam eaque inventore consectetur sapiente itaque aliquid quae tempore. Eveniet iusto obcaecati magni qui laboriosam saepe unde, totam odio excepturi.</p>
+				</article>
+			</div>
+			<div class="col-md-6">
+				<img src="img/aboutimag11.jpg" alt="" class="img-responsive"><br>
+			</div>
+		</div>
+          
+    </div>
+		
+	
+	
+	
+
+	
+
+
+
+	
+	</div><!--  containerFluid Ends -->
+
+
+
+
+	<script src="js/bootstrap.min.js"></script>
+
+
+
+
+
+	
+</body>
+</html>
